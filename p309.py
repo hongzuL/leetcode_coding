9e625f57f6534b2ba690f7b0ff6be8a5d4a66dc7
Hongzu Li <hongzuL@users.noreply.github.com>
2021-10-15 22:45:20 +0000

    not yet finished

--- p309.py
+++ p309.py
@@ -1,12 +1,30 @@
 # 309. Best Time to Buy and Sell Stock with Cooldown
 
+def create_tree(tree, prices):
+    node = 0
+    # root
+    tree.append(node)
+    for 
+    while node < len(prices):
+        
+        while node 
+
 def p309(prices):
     max_profit = 0
     
+    while len(prices) > 0:
+        tree = list()
+        
+        if curr_profit > max_profit:
+            max_profit = curr_profit
+        # remove the first element
+        prices.pop(0)
     return max_profit
 
 def main():
-    prices = [1,2,3,0,2]
-    p309(prices)
+    prices = [2,1,2,1,0,0,1]
+
+    profit = p309(prices)
+    print(profit)
 
 main()
