3a961f62901cd540d1b9c03020d91a2337bb6ff2
Hongzu Li <hongzuL@users.noreply.github.com>
2021-10-15 21:10:14 +0000

    add problem 309

---
+++ 309.py
@@ -0,0 +1,12 @@
+# 309. Best Time to Buy and Sell Stock with Cooldown
+
+def p309(prices):
+    max_profit = 0
+    
+    return max_profit
+
+def main():
+    prices = [1,2,3,0,2]
+    p309(prices)
+
+main()
